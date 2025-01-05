d5e96b5ce173b7c2ec367c2ce4f3dbfd92e5663c
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2025-01-05 17:33:02 +0200

    create product module

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./user.route.js";
 import categoryRouter from "./category.route.js";
+import productRouter from "./product.route.js";
 const router = Router();
 
 router.use("/api", userRouter);
 router.use("/api", categoryRouter);
+router.use("/api", productRouter);
 router.use("*", (req, res) => {
   res.status(404).json({
     message: "Not Found",
